8b6dc81fc74259a54c8547c6b3ac97fed8ec94cc
amanshu-cloud <55711996+amanshu-cloud@users.noreply.github.com>
2020-01-17 01:23:57 +0530

    Add files via upload

--- calculator.py
+++ calculator.py
@@ -23,3 +23,8 @@ if choice == 1:
     num_1 = int(input("enter num1:"))
     num_2 = int(input("enter num2:"))
     print(f"addition of num1 and num2 is {add(num_1,num_2)}")
+
+elif choice == 2:
+    num_1 = int(input("enter subrahend: "))
+    num_2 = int(input("enter subtractor : "))
+    print(f"subtraction of{num_1} and {num_2} is {sub(num_1,num_2)}")
