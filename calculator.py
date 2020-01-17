a54bf73b4b94edd25de62f358ba144fa74493414
amanshu-cloud <55711996+amanshu-cloud@users.noreply.github.com>
2020-01-18 00:25:14 +0530

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -17,7 +17,15 @@ def div(a, b):
     return a/b
 
 
-print("calculator using python\n1-add\n2-subtract\n3-multiply\n4-divide\n")
+def pow(a, b):
+    return a ** b
+
+
+def remainder(a, b):
+    return a % b
+
+
+print("calculator using python\n1-add\n2-subtract\n3-multiply\n4-divide\n5-power\n6-remainder of two nums")
 choice = int(input("enter choice : "))
 if choice == 1:
     num_1 = int(input("enter num1:"))
@@ -38,3 +46,13 @@ elif choice == 4:
     num_1 = int(input("enter dividend :"))
     num_2 = int(input("enter divisor : "))
     print(f"{num_1}/{num_2} is {div(num_1,num_2)}")
+
+elif choice == 5:
+    base = int(input("enter number to be raised: "))
+    power = int(input("what power to raise : "))
+    print(f"{base} to the power {power} = {pow(base,power)}")
+
+elif choice == 6:
+    a = int(input("num_1: "))
+    b = int(input("num_2: "))
+    print(f"remainder of {a} and {b} is {remainder(a,b)}")
